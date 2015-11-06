9260633764ea12d979225b55c4cea3514b3486bb
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2015-11-05 16:43:29 -0800

    cube demo

---
+++ cube/app.js
@@ -0,0 +1,526 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.template = [
+  'svg',
+  {
+    'xmlns:svg': 'http://www.w3.org/2000/svg',
+    xmlns: 'http://www.w3.org/2000/svg',
+    'xmlns:xlink': 'http://www.w3.org/1999/xlink',
+    'xmlns:inkscape': 'http://www.inkscape.org/namespaces/inkscape',
+    width: '450',
+    height: '500',
+    class: 'logo'
+  },
+  [
+    'defs',
+    {id: 'defs4'},
+    [
+      'linearGradient',
+      {id: 'linearGradient5111'},
+      [
+        'stop',
+        {
+          style: 'stop-color:#ffff00;stop-opacity:1;',
+          offset: '0',
+          id: 'stop5113'
+        }
+      ],
+      [
+        'stop',
+        {
+          style: 'stop-color:#00ffff;stop-opacity:0;',
+          offset: '1',
+          id: 'stop5115'
+        }
+      ]
+    ],
+    [
+      'radialGradient',
+      {
+        'inkscape:collect': 'always',
+        'xlink:href': '#linearGradient5111',
+        id: 'radialGradient5119',
+        cx: '276.17346',
+        cy: '538.58264',
+        fx: '276.17346',
+        fy: '538.58264',
+        r: '61.871876',
+        gradientTransform: 'matrix(1,0,0,1.1534505,0,-82.645776)',
+        gradientUnits: 'userSpaceOnUse'
+      }
+    ],
+    [
+      'radialGradient',
+      {
+        'inkscape:collect': 'always',
+        'xlink:href': '#linearGradient5111',
+        id: 'radialGradient5121',
+        gradientUnits: 'userSpaceOnUse',
+        gradientTransform: 'matrix(1,0,0,1.1534505,0,-82.645776)',
+        cx: '276.17346',
+        cy: '538.58264',
+        fx: '276.17346',
+        fy: '538.58264',
+        r: '61.871876'
+      }
+    ],
+    [
+      'radialGradient',
+      {
+        'inkscape:collect': 'always',
+        'xlink:href': '#linearGradient5111',
+        id: 'radialGradient5123',
+        gradientUnits: 'userSpaceOnUse',
+        gradientTransform: 'matrix(1,0,0,1.1534505,0,-82.645776)',
+        cx: '276.17346',
+        cy: '538.58264',
+        fx: '276.17346',
+        fy: '538.58264',
+        r: '61.871876'
+      }
+    ],
+    [
+      'radialGradient',
+      {
+        'inkscape:collect': 'always',
+        'xlink:href': '#linearGradient5111',
+        id: 'radialGradient5125',
+        gradientUnits: 'userSpaceOnUse',
+        gradientTransform: 'matrix(1,0,0,1.1534505,0,-82.645776)',
+        cx: '276.17346',
+        cy: '538.58264',
+        fx: '276.17346',
+        fy: '538.58264',
+        r: '61.871876'
+      }
+    ]
+  ],
+  [
+    'g',
+    {
+      transform: 'translate(-230.14453,-70.87399)',
+      id: '_D'
+    },
+    [
+      'path',
+      {
+        id: 'path4103-7',
+        d: 'm 383.57424,458.85825 76.71485,44.29133 76.71484,-44.29133 -76.71484,-44.29134 z',
+        style: 'fill:#ffffff;fill-opacity:0.50196078;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4143-3',
+        d: 'm 383.57424,476.57478 0,-17.71653 76.71485,44.29133 0,17.71654 z',
+        style: 'fill:#ffffff;fill-opacity:0.50196078;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4145-7',
+        d: 'm 460.28909,520.86612 76.71485,-44.29134 0,-17.71653 -76.71485,44.29133 z',
+        style: 'fill:#ffffff;fill-opacity:0.50196078;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ]
+  ],
+  [
+    'g',
+    {
+      transform: 'translate(-153.42969,-203.74801)',
+      id: '_NE'
+    },
+    [
+      'path',
+      {
+        id: 'path4103-7-5-3-2',
+        d: 'm 444.94612,494.29133 15.34297,8.85825 15.34297,-8.85825 -15.34297,-8.85827 z',
+        style: 'fill:#ff0000;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4143-3-6-5-6',
+        d: 'm 444.94612,512.00786 0,-17.71653 15.34297,8.85825 0,17.71654 z',
+        style: 'fill:#ff0000;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4145-7-5-4-5',
+        d: 'm 460.28909,520.86612 15.34297,-8.85826 0,-17.71653 -15.34297,8.85825 z',
+        style: 'fill:#ff0000;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ]
+  ],
+  [
+    'g',
+    {
+      transform: 'translate(30.685944,-115.16534)',
+      id: '_W'
+    },
+    [
+      'path',
+      {
+        id: 'path4115-3',
+        d: 'm 30.68594,343.70076 76.71485,-44.29134 0,-88.58267 -76.71485,44.29134 z',
+        style: 'fill:#ffffff;fill-opacity:0.50196078;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4139-5',
+        d: 'm 15.34297,246.25982 15.34297,8.85827 0,88.58268 -15.34297,-8.85827 z',
+        style: 'fill:#ffffff;fill-opacity:0.50196078;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4141-3',
+        d: 'm 92.057819,201.96848 15.342971,8.85827 -76.71485,44.29134 -15.34297,-8.85827 z',
+        style: 'fill:#ffffff;fill-opacity:0.50196078;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ]
+  ],
+  [
+    'g',
+    {
+      id: '_N',
+      transform: 'translate(61.371879,-115.16534)'
+    },
+    [
+      'path',
+      {
+        id: 'path4111',
+        d: 'm 352.8883,263.97636 -76.71484,-44.29134 0,88.58268 76.71484,44.29134 z',
+        style: 'fill:#ffffff;fill-opacity:0.50196078;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4113',
+        d: 'm 276.17346,219.68501 15.34297,-8.85826 76.71484,44.29134 -15.34297,8.85826 z',
+        style: 'fill:#ffffff;fill-opacity:0.50196078;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4109',
+        d: 'm 352.8883,352.55904 15.34297,-8.85827 0,-88.58267 -15.34297,8.85826 z',
+        style: 'fill:#ffffff;fill-opacity:0.50196078;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ]
+  ],
+  [
+    'g',
+    {
+      id: '_C',
+      transform: 'translate(-46.028909,-283.47243)',
+      style: 'fill:url(#radialGradient5119);fill-opacity:1'
+    },
+    [
+      'path',
+      {
+        id: 'path4115-3-8',
+        d: 'm 276.17346,609.4488 61.37187,-35.43307 0,-70.86615 -61.37188,35.43308 z',
+        style: 'fill:url(#radialGradient5121);fill-opacity:1;stroke:#000000;stroke-width:1;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-opacity:1;stroke-dasharray:1, 4;stroke-dashoffset:0'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4139-5-3',
+        d: 'm 214.80158,503.14958 61.37188,35.43308 0,70.86614 -61.37188,-35.43307 z',
+        style: 'color:#000000;fill:url(#radialGradient5123);fill-opacity:1;fill-rule:nonzero;stroke:#000000;stroke-width:1;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-opacity:1;stroke-dasharray:1, 4;stroke-dashoffset:0;marker:none;visibility:visible;display:inline;overflow:visible;enable-background:accumulate'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4141-3-4',
+        d: 'm 276.17346,467.71651 61.37187,35.43307 -61.37188,35.43308 -61.37187,-35.43308 z',
+        style: 'color:#000000;fill:url(#radialGradient5125);fill-opacity:1;fill-rule:nonzero;stroke:#000000;stroke-width:1;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-opacity:1;stroke-dasharray:1, 4;stroke-dashoffset:0;marker:none;visibility:visible;display:inline;overflow:visible;enable-background:accumulate'
+      }
+    ]
+  ],
+  [
+    'g',
+    {
+      id: '_S',
+      transform: 'translate(-168.77267,17.70867)'
+    },
+    [
+      'path',
+      {
+        id: 'path4111-2',
+        d: 'm 276.17346,308.2677 -76.71484,-44.29134 0,88.58268 76.71484,44.29134 z',
+        style: 'fill:#ffffff;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4113-5',
+        d: 'm 199.45862,263.97635 15.34297,-8.85826 76.71484,44.29134 -15.34297,8.85826 z',
+        style: 'fill:#ffffff;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4109-1',
+        d: 'm 276.17346,396.85038 15.34297,-8.85827 0,-88.58267 -15.34297,8.85826 z',
+        style: 'fill:#ffffff;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ]
+  ],
+  [
+    'g',
+    {
+      transform: 'translate(306.8594,79.71655)',
+      id: '_E'
+    },
+    [
+      'path',
+      {
+        id: 'path4115-3-6',
+        d: 'm 30.68594,343.70076 76.71485,-44.29134 0,-88.58267 -76.71485,44.29134 z',
+        style: 'fill:#ffffff;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4139-5-5',
+        d: 'm 15.34297,246.25982 15.34297,8.85827 0,88.58268 -15.34297,-8.85827 z',
+        style: 'fill:#ffffff;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4141-3-2',
+        d: 'm 92.057819,201.96848 15.342971,8.85827 -76.71485,44.29134 -15.34297,-8.85827 z',
+        style: 'fill:#ffffff;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ]
+  ],
+  [
+    'g',
+    {
+      transform: 'translate(-306.85938,-292.33069)',
+      id: '_SW'
+    },
+    [
+      'path',
+      {
+        id: 'path4103-7-5-3',
+        d: 'm 444.94612,494.29133 15.34297,8.85825 15.34297,-8.85825 -15.34297,-8.85827 z',
+        style: 'fill:#ff0000;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4143-3-6-5',
+        d: 'm 444.94612,512.00786 0,-17.71653 15.34297,8.85825 0,17.71654 z',
+        style: 'fill:#ff0000;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4145-7-5-4',
+        d: 'm 460.28909,520.86612 15.34297,-8.85826 0,-17.71653 -15.34297,8.85825 z',
+        style: 'fill:#ff0000;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ]
+  ],
+  [
+    'g',
+    {
+      transform: 'translate(-230.14453,-372.0551)',
+      id: '_U'
+    },
+    [
+      'path',
+      {
+        id: 'path4103-7-5',
+        d: 'm 383.57424,458.85825 76.71485,44.29133 76.71484,-44.29133 -76.71484,-44.29134 z',
+        style: 'fill:#ffffff;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4143-3-6',
+        d: 'm 383.57424,476.57478 0,-17.71653 76.71485,44.29133 0,17.71654 z',
+        style: 'fill:#ffffff;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ],
+    [
+      'path',
+      {
+        id: 'path4145-7-5',
+        d: 'm 460.28909,520.86612 76.71485,-44.29134 0,-17.71653 -76.71485,44.29133 z',
+        style: 'fill:#ffffff;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1'
+      }
+    ]
+  ]
+];
+exports.moves = {
+  E: [
+    107.40078,
+    62.00788
+  ],
+  N: [
+    107.40078999999997,
+    -62.00787
+  ],
+  U: [
+    0,
+    -124.01574
+  ],
+  W: [
+    -107.400788,
+    -62.00787
+  ],
+  S: [
+    -107.400788,
+    62.00788
+  ],
+  D: [
+    0,
+    106.29921
+  ]
+};
+
+},{}],2:[function(require,module,exports){
+'use strict';
+
+var onmls = require('onml/lib/stringify'),
+    assets = require('./assets');
+
+var moves = assets.moves;
+var root = document.getElementById('content');
+root.innerHTML = onmls(assets.template);
+
+Object.keys(moves).forEach(function (e) {
+    var el = document.getElementById('_' + e);
+    el.addEventListener('click', (function (name) {
+        var dir = 1;
+        var scale = 0;
+        var origin = el.getAttribute('transform').split(/[(),]/);
+        var delta = moves[name];
+        var inter = setInterval(function () {
+            if (dir === 1) {
+                if (scale < 0.9) {
+                    scale += 0.1;
+                }
+            } else {
+                if (scale > 0.1) {
+                    scale -= 0.1;
+                }
+            }
+            // el.setAttribute('transform', 'translate(' + origin[1] + ',' + origin[2] + ')');
+            el.setAttribute(
+                'transform', 'translate(' +
+                (Number(origin[1]) + (1 - scale * delta[0])) +
+                ',' +
+                (Number(origin[2]) + (1 - scale * delta[1])) +
+                ')'
+            );
+        }, 100);
+        return function () {
+            console.log(scale);
+            if (dir === 0) {
+                dir = 1;
+            } else {
+                dir = 0;
+            }
+        };
+    })(e));
+});
+
+},{"./assets":1,"onml/lib/stringify":3}],3:[function(require,module,exports){
+'use strict';
+
+function isObject (o) {
+    return o && Object.prototype.toString.call(o) === '[object Object]';
+}
+
+function indent (txt) {
+    var arr, res = [];
+
+    if (typeof txt !== 'string') {
+        return txt;
+    }
+
+    arr = txt.split('\n');
+
+    if (arr.length === 1) {
+        return '  ' + txt;
+    }
+
+    arr.forEach(function (e) {
+        if (e === '') {
+            res.push(e);
+            return;
+        }
+        res.push('  ' + e);
+    });
+
+    return res.join('\n');
+}
+
+function stringify (a) {
+    var res, body;
+
+    body = '';
+    if (a.some(function (e, i, arr) {
+        if (i === 0) {
+            res = '<' + e;
+            if (arr.length === 1) {
+                return true;
+            }
+            return;
+        }
+
+        if (i === 1) {
+            if (isObject(e)) {
+                Object.keys(e).forEach(function (key) {
+                    res += ' ' + key + '="' + e[key] + '"';
+                });
+                if (arr.length === 2) {
+                    return true;
+                }
+                res += '>\n';
+                return;
+            } else {
+                res += '>\n';
+            }
+        }
+
+        if (typeof e === 'string') {
+            body += e + '\n';
+            return;
+        }
+
+        body += stringify(e);
+    })) {
+        return res + '/>\n'; // short form
+    } else {
+        return res + indent(body) + '</' + a[0] + '>\n';
+    }
+}
+
+module.exports = stringify;
+
+},{}]},{},[2]);
