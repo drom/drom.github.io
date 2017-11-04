828d7dbc9b905eec28cd641bb4137c3386c2c076
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-11-03 23:00:21 -0700

    arrArr splitter

--- cosmo/app.js
+++ cosmo/app.js
@@ -12,34 +12,37 @@ const objects = {
     '+': 'PwrLine4'
 };
 
-function trimReduce (res, e, y) {
-    if (e.trim() !== '') {
-        const row = e.split('');
-        console.log(row);
-        const row1 = row.reduce((res1, e, x) => {
-            if (objects[e]) {
-                let cell = clone(skin[objects[e]]);
-                cell[1].transform = `translate(${20 * x},${20 * y})`;
-                res1.push(cell);
-            }
-            return res1;
-        }, ['g']);
-        res.push(row1);
-    }
+function trimReduce (res, row, y) {
+    console.log(row);
+    const row1 = row.reduce((res1, e, x) => {
+        if (objects[e]) {
+            let cell = clone(skin[objects[e]]);
+            cell[1].transform = `translate(${20 * x},${20 * y})`;
+            res1.push(cell);
+        }
+        return res1;
+    }, ['g']);
+    res.push(row1);
     return res;
 }
 
-function shipParser (str, num) {
-    const map = str.split('\n');
-    // console.log(map);
-    return map.reduce(trimReduce, ['g', {
+function shipParser (arr, num) {
+    return arr.reduce(trimReduce, ['g', {
         transform: `translate(0,${256 * num})`
     }]);
 }
 
+const arrArr = str => {
+    const arr = str.split('\n\n\n').map(
+        ship => ship.split('\n').map(
+            row => row.split('')
+        )
+    );
+    return arr;
+};
+
 function asciiParser (str) {
-    const body = str.split('\n\n\n').map(shipParser);
-    // return onml.stringify(['svg'].concat(body));
+    const body = arrArr(str).map(shipParser);
     return onml.stringify(['svg', {
         xmlns: 'http://www.w3.org/2000/svg',
         'xmlns:inkscape': 'http://www.inkscape.org/namespaces/inkscape',
